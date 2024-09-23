214b501b61629790575f0197aef48e096b7663d1
venkatraman3103 <venkat.raman.3103@gmail.com>
2024-09-24 00:27:27 +0530

    update

--- 1-Basics/1.4-Sorting/1.4.5-Merge-Sort/1.4.5-Merge-Sort.cpp
+++ 1-Basics/1.4-Sorting/1.4.5-Merge-Sort/1.4.5-Merge-Sort.cpp
@@ -5,24 +5,62 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> mergeSort(int start, int end, vector<int> arr)
+    void mergeSort(int start, int end, vector<int>& arr)
     {
-        int mid = arr.size() / 2;
-        
+
         // base case
         if (start >= end)
         {
-            return arr;
+            return;
         }
+        int mid = start + (end - start) / 2;
 
-        mergeSort(end, mid, arr);
+        mergeSort(start, mid, arr);
         mergeSort(mid + 1, end, arr);
 
-        return arr;
+        merging(start, mid, end, arr);
     }
+
+    void merging(int start, int mid, int end, vector<int>& arr)
+    {
+        vector<int> tempArr;
+
+        int left = start;
+        int right = mid + 1;
+
+        while (left <= mid && right <= end)
+        {
+            if (arr[left] <= arr[right])
+            {
+                tempArr.push_back(arr[left]);
+                left++;
+            }
+            else {
+                tempArr.push_back(arr[right]);
+                right++;
+            };
+        }
+
+        while (left <= mid)
+        {
+            tempArr.push_back(arr[left]);
+            left++;
+        }
+
+        while (right <= end)
+        {
+            tempArr.push_back(arr[right]);
+            right++;
+        }
+
+        for (int i = start; i <= end; i++)
+        {
+            arr[i] = tempArr[i - start];
+        }
+    };
 };
 
-void PrintAnArray(vector<int> arr)
+void PrintAnArray(vector<int>& arr)
 {
     for (int n : arr)
     {
@@ -36,5 +74,7 @@ int main()
 
     Solution solution;
 
-    PrintAnArray(solution.mergeSort(0, arr.size()-1, arr));
+    solution.mergeSort(0, arr.size() - 1, arr);
+
+    PrintAnArray(arr);
 }
