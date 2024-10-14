6ddc2f601181eddd2640c8a6540b119310f88173
venkatraman3103 <venkat.raman.3103@gmail.com>
2024-10-14 23:00:43 +0530

    update

--- 1-Basics/1.4-Sorting/1.4.1-Bubble-sort/1.4.1-Bubble-sort.cpp
+++ 1-Basics/1.4-Sorting/1.4.1-Bubble-sort/1.4.1-Bubble-sort.cpp
@@ -5,22 +5,20 @@ using namespace std;
 class Solution
 {
 public:
-
     vector<int> bubbleSort(vector<int> arr)
     {
-        for (int i = 0; i <= arr.size() - 1; i++)
+        for (int i = 0; i < arr.size(); i++)
         {
-            for (int j = 0; j <= (arr.size() - i) - 1; j++)
+            for (int j = 0; j < arr.size() - i; j++)
             {
                 if (arr[j] > arr[j + 1])
                 {
                     // swapping
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            cout << endl;
         }
         return arr;
     }
