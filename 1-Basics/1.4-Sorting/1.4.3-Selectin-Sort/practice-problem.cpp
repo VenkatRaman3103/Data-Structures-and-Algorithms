edb4fde81fd7e832fe744afe4cf139bc610008ac
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-10-27 22:24:24 +0530

    update

---
+++ 1-Basics/1.4-Sorting/1.4.3-Selectin-Sort/practice-problem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> insertionSort(vector<int> nums) {
+
+    for (int i = 0; i < nums.size(); i++) {
+
+        int min = i;
+
+        for (int j = i; j < nums.size(); j++) {
+
+            if (nums[j] < nums[min]) {
+                min = j;
+            }
+
+            // swap
+            int temp = nums[min];
+            nums[min] = nums[i];
+            nums[i] = temp;
+        }
+    }
+
+    return nums;
+};
+
+void printAnArray(vector<int> nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << endl;
+    };
+};
+
+int main() { printAnArray(insertionSort({2, 3, 1, 4, 5})); }
