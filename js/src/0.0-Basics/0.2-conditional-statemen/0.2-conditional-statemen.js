beff6821bf904ed3a1b6f5534c5545751f402e44
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-03-29 22:44:43 +0530

    COMPLETE: Write a program to check if a number is even or odd - js

---
+++ js/src/0.0-Basics/0.2-conditional-statemen/0.2-conditional-statemen.js
@@ -0,0 +1,52 @@
+import readline from 'readline-sync';
+
+function getUserInput(bool) {
+    let userInput;
+
+    if (bool) {
+        userInput = readline.question();
+    }
+    return userInput;
+}
+
+// NOTE: Basic Level
+
+// COMPLETE: Write a program to check if a number is even or odd
+function isEven(num) {
+    let numType;
+
+    let remainder = num % 2;
+
+    if (remainder == 0) {
+        numType = true;
+    } else {
+        numType = false;
+    }
+
+    let output = `${num} is ${numType ? 'Even' : 'Odd'}`;
+
+    console.log(output);
+}
+
+// isEven(getUserInput(true));
+
+// TODO: Write a program to check if a number is positive, negative, or zero
+function signCheck(num) {}
+
+// TODO: Check if a person is eligible to vote (age >= 18)
+// TODO: Find the greater of two numbers
+// TODO: Check if a number is divisible by both 5 and 11
+
+// NOTE: Intermediate Level
+// TODO: Implement a leap year checker
+// TODO: Create a grade calculator (A, B, C, D, F based on marks)
+// TODO: Check if three given sides form a valid triangle
+// TODO: Identify if a character is a vowel, consonant, digit, or special character
+// TODO: Build a simple calculator (+, -, *, /)
+
+// NOTE: Advanced Level
+// TODO: Find the largest of three numbers
+// TODO: Implement a profit or loss calculator
+// TODO: Write an electricity bill calculator with tiered pricing
+// TODO: Determine the type of triangle (Equilateral, Isosceles, Scalene)
+// TODO: Map numbers 1-7 to corresponding days of the week
