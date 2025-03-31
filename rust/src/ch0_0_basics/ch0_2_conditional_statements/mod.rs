8da630ee71ac14f7b95972ced6255f7204c489b3
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-03-31 19:30:06 +0530

    COMPLETE: Write a program to check if a number is even or odd - rust COMPLETE: Write a program to check if a number is positive, negative, or zero - rust COMPLETE: Check if a person is eligible to vote (age >= 18) - rust

--- rust/src/ch0_0_basics/ch0_2_conditional_statements/mod.rs
+++ rust/src/ch0_0_basics/ch0_2_conditional_statements/mod.rs
@@ -16,7 +16,7 @@ fn get_user_input() -> std::string::String {
 
 // NOTE: Basic Level
 
-// TODO: Write a program to check if a number is even or odd
+// COMPLETE: Write a program to check if a number is even or odd
 pub fn is_even() {
     let num: i32 = get_user_input().parse().expect("Invalid type");
 
@@ -33,9 +33,51 @@ pub fn is_even() {
     }
 }
 
-// TODO: Write a program to check if a number is positive, negative, or zero
-// TODO: Check if a person is eligible to vote (age >= 18)
+// COMPLETE: Write a program to check if a number is positive, negative, or zero
+pub fn check_sign() {
+    let user_input: i32 = get_user_input()
+        .parse()
+        .expect("error in parsing the user input");
+
+    match user_input {
+        user_input if user_input > 0 => println!("{} is positive integer", user_input),
+        user_input if user_input < 0 => println!("{} is negative integer", user_input),
+        _ => println!("{} is zero", user_input),
+    }
+}
+
+// COMPLETE: Check if a person is eligible to vote (age >= 18)
+pub fn votingEligibility() {
+    let age: i32 = get_user_input()
+        .parse()
+        .expect("error in parsing the user input");
+
+    if age >= 18 {
+        println!("you are eligibile for voting")
+    } else {
+        println!("you are not eligible for voting")
+    }
+}
+
 // TODO: Find the greater of two numbers
+pub fn find_greatest() {
+    let num_1: i32 = get_user_input()
+        .parse()
+        .expect("error in parsing the user input 1");
+
+    let num_2: i32 = get_user_input()
+        .parse()
+        .expect("error in parsing the user input 2");
+
+    if num_2 > num_1 {
+        println!("{} is greater than {}", num_2, num_1)
+    } else if num_1 > num_2 {
+        println!("{} is greater than {}", num_1, num_2)
+    } else {
+        println!("bot {} and {} are equal", num_1, num_2)
+    }
+}
+
 // TODO: Check if a number is divisible by both 5 and 11
 
 // NOTE: Intermediate Level
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,8 @@
 mod ch0_0_basics;
 
 fn main() {
-    ch0_0_basics::ch0_2_conditional_statements::is_even();
+    // ch0_0_basics::ch0_2_conditional_statements::is_even();
+    ch0_0_basics::ch0_2_conditional_statements::check_sign();
+    // ch0_0_basics::ch0_2_conditional_statements::votingEligibility();
+    // ch0_0_basics::ch0_2_conditional_statements::find_greatest();
 }
