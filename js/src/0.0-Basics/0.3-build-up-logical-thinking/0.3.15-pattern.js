ee1057b43e6647fad6a504e311a931886d05bb28
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-06 00:48:16 +0530

    complete pattern 15 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.15-pattern.js
@@ -0,0 +1,25 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+
+function pattern_15() {
+    let row = 5;
+    let column = 5;
+
+    let arr = ['a', 'b', 'c', 'd', 'e'];
+
+    for (let y = 0; y < row; y++) {
+        for (let x of arr) {
+            let ind = arr.indexOf(x);
+
+            if (ind <= y) {
+                log(x);
+            } else {
+                log(' ');
+            }
+        }
+        console.log();
+    }
+}
+
+pattern_15();
