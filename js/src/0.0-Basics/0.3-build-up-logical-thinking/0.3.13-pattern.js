329508f56be3b400d201667a08ee2e456731ae3f
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-06 00:10:03 +0530

    complete pattern 13 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.13-pattern.js
@@ -0,0 +1,32 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+
+function patter_13() {
+    let row = 5;
+    let column = 10;
+
+    for (let y = 1; y < row; y++) {
+        for (let x = 1; x < column; x++) {
+            if (x <= column / 2) {
+                if (x <= y) {
+                    log(x);
+                } else {
+                    log(' ');
+                }
+            } else {
+                let key = column - x;
+
+                if (key <= y) {
+                    log(key);
+                } else {
+                    log(' ');
+                }
+            }
+        }
+
+        console.log();
+    }
+}
+
+patter_13();
