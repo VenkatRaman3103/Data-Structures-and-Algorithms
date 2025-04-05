194d8f6aaef6b1c644e7728760f5f7b0881f20a3
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-05 13:29:05 +0530

    complete pattern 6 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.6-pattern.js
@@ -0,0 +1,14 @@
+function pattern_6() {
+    let row = 5;
+    let colums = 5;
+
+    for (let y = 5; y > 0; y--) {
+        for (let x = 1; x <= y; x++) {
+            process.stdout.write(String(x));
+        }
+
+        console.log();
+    }
+}
+
+pattern_6();
