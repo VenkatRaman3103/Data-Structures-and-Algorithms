48f3fbfe7134e1e0ec00a41dd7c866c4a6350002
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-05 19:42:47 +0530

    complete pattern 9 - js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.9-pattern.js
@@ -0,0 +1,31 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+
+function patter_9() {
+    let row = 5;
+    let column = 10;
+
+    for (let y = 1; y < row + 1; y++) {
+        for (let x = 1; x < column; x++) {
+            if (x <= column - column / 2) {
+                if (x >= y) {
+                    log('*');
+                } else {
+                    log(' ');
+                }
+            } else {
+                let key = column - x;
+
+                if (key >= y) {
+                    log('*');
+                } else {
+                    log(' ');
+                }
+            }
+        }
+        console.log();
+    }
+}
+
+patter_9();
