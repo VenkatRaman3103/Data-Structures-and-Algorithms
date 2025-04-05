d1c88b0a6de204ea7164eae95cc96d6f0d098b1e
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-05 14:59:48 +0530

    complete pattern 7 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.7-pattern.js
@@ -0,0 +1,17 @@
+function pattern_7() {
+    let row = 5;
+    let column = 5;
+
+    for (let y = 1; y < row + 1; y++) {
+        for (let x = 5; x > 0; x--) {
+            if (x <= y) {
+                process.stdout.write(String(x));
+            } else {
+                process.stdout.write(' ');
+            }
+        }
+        console.log();
+    }
+}
+
+pattern_7();
