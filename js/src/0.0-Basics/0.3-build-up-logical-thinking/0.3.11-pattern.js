22a3075fa7c33082154d9643122697e588cfca05
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-05 22:08:39 +0530

    complete patter 11 in js

--- js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.11-pattern.js
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.11-pattern.js
@@ -4,7 +4,27 @@ function log(val) {
 //
 
 function pattern_11() {
-    //
+    let row = 10;
+    let column = 5;
+
+    for (let y = 1; y < row; y++) {
+        for (let x = 1; x < column + 1; x++) {
+            if (y <= row / 2) {
+                if (x <= y) {
+                    log('*');
+                } else {
+                    log(' ');
+                }
+            } else {
+                if (x <= row - y) {
+                    log('*');
+                } else {
+                    log(' ');
+                }
+            }
+        }
+        console.log();
+    }
 }
 
 pattern_11();
