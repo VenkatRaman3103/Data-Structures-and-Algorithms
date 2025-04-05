6d9df88eaff6ce84fb489b3c973b3f99d6afd71d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-06 00:00:03 +0530

    complete pattern 12 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.12-pattern.js
@@ -0,0 +1,28 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+
+function pattern_12() {
+    let row = 5;
+    let column = 5;
+
+    let addRow = 0;
+
+    for (let y = 1; y < row + 1; y++) {
+        let addColumn = y + 1;
+        for (let x = 1; x < y + 1; x++) {
+            addColumn = addColumn - 1;
+
+            if (addColumn % 2 == 0) {
+                log('0');
+            } else {
+                log('1');
+            }
+        }
+        addRow = addRow + 1;
+
+        console.log();
+    }
+}
+
+pattern_12();
