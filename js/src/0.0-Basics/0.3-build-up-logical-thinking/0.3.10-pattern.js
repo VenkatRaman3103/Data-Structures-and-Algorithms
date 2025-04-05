53c864a8ebce476c41625d9780bc91391305b932
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-05 20:42:02 +0530

    complete pattern 10 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.10-pattern.js
@@ -0,0 +1,47 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+function patter_10() {
+    let row = 10;
+    let column = 10;
+
+    for (let y = row; y > 0; y--) {
+        for (let x = 1; x < column; x++) {
+            if (x <= column / 2) {
+                if (y >= row / 2) {
+                    if (x <= y - row / 2) {
+                        log(' ');
+                    } else {
+                        log('*');
+                    }
+                } else if (y < row / 2) {
+                    if (x > row / 2 - y) {
+                        log('*');
+                    } else {
+                        log(' ');
+                    }
+                }
+            } else {
+                let key = column - x;
+
+                if (y >= row / 2) {
+                    if (key <= y - row / 2) {
+                        log(' ');
+                    } else {
+                        log('*');
+                    }
+                } else if (y < row / 2) {
+                    if (key > row / 2 - y) {
+                        log('*');
+                    } else {
+                        log(' ');
+                    }
+                }
+            }
+        }
+
+        console.log();
+    }
+}
+
+patter_10();
