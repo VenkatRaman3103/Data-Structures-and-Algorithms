34a206b0088fd11a1ac43028e5d12f2bc7010023
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-06 17:06:01 +0530

    complete pattern 21 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.21-pattern.js
@@ -0,0 +1,26 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+
+function pattern_21() {
+    let column = 5;
+    let row = 5;
+
+    for (let y = 1; y < row + 1; y++) {
+        let counter = 1;
+
+        for (let x = column; x > 0; x--) {
+            if (x <= y) {
+                log(counter);
+
+                counter = counter + 1;
+            } else {
+                log(' ');
+            }
+        }
+        counter = 1;
+        console.log();
+    }
+}
+
+pattern_21();
