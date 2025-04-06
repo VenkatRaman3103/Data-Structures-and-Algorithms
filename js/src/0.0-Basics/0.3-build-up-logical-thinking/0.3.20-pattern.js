50f74905cf69271c0742bb5c95c2229fa3ae5bc9
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-06 15:47:31 +0530

    update

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.20-pattern.js
@@ -0,0 +1,32 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+
+function pattern_19() {
+    let chars = ['a', 'b', 'c', 'd', 'e'];
+
+    let row = 5;
+    let column = 10;
+
+    for (let y = row - 1; y >= 0; y--) {
+        for (let x = column; x >= 0; x--) {
+            if (x <= column / 2) {
+                if (x > y) {
+                    log(chars[x - 1]);
+                } else {
+                    log(' ');
+                }
+            } else if (x - 1 > column / 2) {
+                let element = column - x;
+
+                if (element >= y) {
+                    log(chars[element]);
+                } else {
+                    log(' ');
+                }
+            }
+        }
+    }
+}
+
+pattern_19();
