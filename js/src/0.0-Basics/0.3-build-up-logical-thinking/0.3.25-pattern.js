c456a87382f0726d792c244da4ba2a199442ca9e
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-07 08:06:23 +0530

    complete pattern 25 in js

---
+++ js/src/0.0-Basics/0.3-build-up-logical-thinking/0.3.25-pattern.js
@@ -0,0 +1,50 @@
+function log(val) {
+    process.stdout.write(String(val));
+}
+
+function pattern_25() {
+    let row = 10;
+    let column = 10;
+
+    for (let y = 1; y < row + 1; y++) {
+        for (let x = 1; x <= column; x++) {
+            if (x <= column / 2) {
+                if (y <= row / 2) {
+                    if (x <= y) {
+                        log(x);
+                    } else {
+                        log(' ');
+                    }
+                } else if (y >= row / 2) {
+                    if (x <= row - y) {
+                        log(x);
+                    } else {
+                        log(' ');
+                    }
+                }
+            } else if (x > column / 2) {
+                let key = column - x + 1;
+                if (y <= row / 2) {
+                    if (key <= y) {
+                        log(key);
+                    } else {
+                        log(' ');
+                    }
+                } else if (y >= row / 2) {
+                    if (key <= row - y) {
+                        log(key);
+                    } else {
+                        log(' ');
+                    }
+                }
+            }
+        }
+
+        console.log(' ', y);
+    }
+
+    console.log();
+    return 0;
+}
+
+pattern_25();
