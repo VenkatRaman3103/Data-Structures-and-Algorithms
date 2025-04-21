1abf39ebf5deb49b635fb4454c7c7087e34711a1
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-04-21 22:13:24 +0530

    - complete count-trailing zeros in factorial in js

--- js/src/0.0-Basics/0.4-math/0.4.12-count-trailing-zeros-in-factorial.js
+++ js/src/0.0-Basics/0.4-math/0.4.12-count-trailing-zeros-in-factorial.js
@@ -1,4 +1,4 @@
-function factorialOfNum(num) {
+function factorialNum(num) {
     let accumulator = 1;
 
     for (let i = 1; i <= num; i++) {
@@ -8,5 +8,34 @@ function factorialOfNum(num) {
     return accumulator;
 }
 
+function trialingZeros_1(num) {
+    let factorial = factorialNum(num);
+
+    let countZeros = 0;
+
+    while (factorial > 0) {
+        let lastNum = factorial % 10;
+        if (lastNum == 0) {
+            countZeros = countZeros + 1;
+        } else {
+            break;
+        }
+        factorial = Math.floor(factorial / 10);
+    }
+
+    return countZeros;
+}
+
+function trialingZeros_2(num) {
+    let counter = 0;
+
+    for (let i = 5; num / i >= 1; i *= 5) {
+        counter += num / i;
+    }
+
+    return counter;
+}
+
 console.clear();
-factorialOfNum(5);
+trialingZeros_1(15);
+trialingZeros_2(15);
