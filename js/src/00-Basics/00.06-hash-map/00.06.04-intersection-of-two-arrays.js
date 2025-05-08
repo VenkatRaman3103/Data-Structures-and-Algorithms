b3ca4bb077e474c65cf15ea0b51ce8a10934a794
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-05-08 09:13:08 +0530

    update

--- js/src/00-Basics/00.06-hash-map/00.06.04-intersection-of-two-arrays.js
+++ js/src/00-Basics/00.06-hash-map/00.06.04-intersection-of-two-arrays.js
@@ -27,30 +27,30 @@ const arr_2 = [2, 2];
 
 // NOTE: Intersection of Two Arrays using hashmap
 function intersectionOfArrays_hashmap(nums1, nums2) {
-    let table_1 = {};
+    let hasTable = {};
     let result = [];
 
     for (let i = 0; i < nums1.length; i++) {
-        let key = nums1[i];
+        const key = nums1[i];
 
-        if (table_1[key] == undefined) {
-            table_1[key] = 1;
+        if (hasTable[key] == undefined) {
+            hasTable[key] = 1;
         } else {
-            table_1[key] += 1;
+            hasTable[key] += 1;
         }
     }
 
-    for (let i = 0; i < nums2.length; i++) {
-        let key = nums2[i];
+    for (let j = 0; j < nums2.length; j++) {
+        let element = nums2[j];
 
-        if (table_1[key] > 0) {
-            result.push(key);
-            table_1[key] -= 1;
+        if (hasTable[element] > 0) {
+            result.push(element);
+            hasTable[element] -= 1;
         }
     }
 
     return result;
 }
 
-console.clear();
-intersectionOfArrays_hashmap(arr_1, arr_2);
+const result = intersectionOfArrays_hashmap(arr_1, arr_2);
+console.log(result);
