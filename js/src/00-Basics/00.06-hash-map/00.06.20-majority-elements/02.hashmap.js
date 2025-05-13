c4bc24de1419a86a719fb710bf93760c448c57e7
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-05-13 23:01:38 +0530

    update

---
+++ js/src/00-Basics/00.06-hash-map/00.06.20-majority-elements/02.hashmap.js
@@ -0,0 +1,30 @@
+function majorityElement(nums) {
+    let hashMap = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let key = nums[i];
+
+        if (hashMap[key] == undefined) {
+            hashMap[key] = 1;
+        } else {
+            hashMap[key] += 1;
+        }
+    }
+
+    let largest = hashMap[nums[0]];
+    let result = nums[0];
+
+    for (let key of Object.keys(hashMap)) {
+        if (hashMap[key] > largest) {
+            largest = hashMap[key];
+            result = Number(key);
+        }
+    }
+
+    console.log(hashMap);
+    return result;
+}
+
+const nums = [3, 2, 1, 1, 1, 1, 1, 1, 1, 3, 2, 2, 2, 2];
+const result = majorityElement(nums);
+console.log(result);
