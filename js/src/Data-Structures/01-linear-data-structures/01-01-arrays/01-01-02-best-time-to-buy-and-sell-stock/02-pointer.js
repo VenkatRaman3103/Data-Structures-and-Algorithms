05f3851567e0de88131b0c73807da3d168810f3b
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-05-29 22:24:14 +0530

    update

---
+++ js/src/Data-Structures/01-linear-data-structures/01-01-arrays/01-01-02-best-time-to-buy-and-sell-stock/02-pointer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let l = 0;
+    let max = 0;
+
+    for (let r = 1; r < prices.length; r++) {
+        if (prices[l] < prices[r]) {
+            let profit = prices[r] - prices[l];
+
+            max = Math.max(profit, max);
+        } else {
+            l = r;
+        }
+    }
+
+    return max;
+};
+
+const prices = [7, 1, 5, 3, 6, 4];
+const result = maxProfit(prices);
+console.log(result);
