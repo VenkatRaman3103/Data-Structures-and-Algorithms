66c8055c7be1a5687cb629e30ccdc1cb88fc156d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-08-07 22:18:41 +0530

    update

---
+++ js/src/Revision/pattern_printing/04-pattern.js
@@ -0,0 +1,15 @@
+const log = (value) => {
+    process.stdout.write(String(value));
+};
+
+const pattern_4 = (n) => {
+    for (let i = 1; i < n + 1; i++) {
+        for (let j = 1; j <= i; j++) {
+            log(j);
+        }
+
+        console.log();
+    }
+};
+
+pattern_4(5);
