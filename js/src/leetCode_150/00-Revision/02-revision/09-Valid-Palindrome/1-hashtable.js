8ec55580152a6992f8397a77f2a7abe34aa7ff0e
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-12-07 06:54:18 +0000

    update

--- js/src/leetCode_150/00-Revision/02-revision/09-Valid-Palindrome/1-hashtable.js
+++ js/src/leetCode_150/00-Revision/02-revision/09-Valid-Palindrome/1-hashtable.js
@@ -1,8 +1,34 @@
-var isPalindrome = function (s) {
+export var isPalindrome = function (s) {
     let chars = s.split('');
-    console.log(chars);
+
+    let l = 0;
+    let r = chars.length - 1;
+
+    while (l < r) {
+        let charAt_l = chars[l].toLowerCase();
+        let charAt_r = chars[r].toLowerCase();
+
+        if (!/[a-z0-9]/.test(charAt_l)) {
+            l++;
+            continue;
+        }
+
+        if (!/[a-z0-9]/.test(charAt_r)) {
+            r--;
+            continue;
+        }
+
+        if (charAt_l == charAt_r) {
+            r--;
+            l++;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 const s = 'A man, a plan, a canal: Panama';
 const result = isPalindrome(s);
-console.log(s);
+console.log(result);
