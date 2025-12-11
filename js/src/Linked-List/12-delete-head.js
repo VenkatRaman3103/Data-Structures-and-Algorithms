441666a04910bfbad0e55df02b053c57dadc6167
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-12-11 06:00:20 +0000

    update

---
+++ js/src/Linked-List/12-delete-head.js
@@ -0,0 +1,59 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        let newNode = new Node(value);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+    }
+
+    deleteHead() {
+        let temp = this.head.next;
+        this.head = temp;
+    }
+
+    print() {
+        let arr = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            arr.push(curr.value);
+            curr = curr.next;
+        }
+
+        return arr;
+    }
+}
+
+const linkedList = new LinkedList();
+
+const nums = [10, 20, 30];
+
+for (let n of nums) {
+    linkedList.append(n);
+}
+
+linkedList.deleteHead();
+const list = linkedList.print();
+console.log(list);
