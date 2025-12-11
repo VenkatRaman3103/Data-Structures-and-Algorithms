991e65cd61b8f1c161af536f4533579df50e3361
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-12-11 02:43:47 +0000

    update

---
+++ js/src/rough.js
@@ -0,0 +1,70 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+    }
+
+    reverse_stack() {
+        let stack = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            stack.push(curr.value);
+            curr = curr.next;
+        }
+
+        this.head = null;
+
+        while (stack.length > 0) {
+            this.append(stack.pop());
+        }
+    }
+
+    toAnArray() {
+        let arr = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            arr.push(curr.value);
+            curr = curr.next;
+        }
+
+        return arr;
+    }
+}
+
+const linkedList = new LinkedList();
+
+const nums = [1, 2, 3, 4, 5];
+
+for (let n of nums) {
+    linkedList.append(n);
+}
+
+linkedList.reverse_stack();
+console.log(linkedList.toAnArray());
