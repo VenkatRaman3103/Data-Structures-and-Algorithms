6588fe8613d01a72f5f090463e5e0bc3684aff86
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-12-13 12:08:38 +0530

    update

---
+++ js/src/Linked-List/19-return-middle-node-pointers.js
@@ -0,0 +1,68 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        let newNode = new Node(value);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+
+        this.length++;
+    }
+
+    midElement() {
+        let slow = this.head;
+        let fast = this.head;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.value;
+    }
+
+    print() {
+        let arr = [];
+
+        let current = this.head;
+
+        while (current != null) {
+            arr.push(current.value);
+            current = current.next;
+        }
+
+        console.log(arr);
+    }
+}
+
+const linkedList = new LinkedList();
+
+const nums = [1, 2, 3, 4, 5];
+
+for (let n of nums) {
+    linkedList.append(n);
+}
+
+// linkedList.print();
+const result = linkedList.midElement();
+console.log(result);
