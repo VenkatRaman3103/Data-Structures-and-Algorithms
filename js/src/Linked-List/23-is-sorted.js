6725a77f5948b0c88d44ac6a507c4cf94be60459
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-12-15 10:40:40 +0530

    update

---
+++ js/src/Linked-List/23-is-sorted.js
@@ -0,0 +1,70 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        let newNode = new Node(value);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+
+        this.length++;
+    }
+
+    isSorted() {
+        let current = this.head;
+
+        while (current.next != null) {
+            if (current.value <= current.next.value) {
+                current = current.next;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    print() {
+        let arr = [];
+
+        let current = this.head;
+
+        while (current != null) {
+            arr.push(current.value);
+            current = current.next;
+        }
+
+        console.log(arr);
+    }
+}
+
+const linkedList = new LinkedList();
+
+const nums = [1, 2, 3, 4, 5];
+
+for (let n of nums) {
+    linkedList.append(n);
+}
+
+linkedList.print();
+const result = linkedList.isSorted();
+console.log(result);
