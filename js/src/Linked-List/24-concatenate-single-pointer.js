9a77bd86411af5b5431a8da1915e748bba89804d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-12-15 17:45:48 +0530

    update

--- js/src/Linked-List/24-concatenate-single-pointer.js
+++ js/src/Linked-List/24-concatenate-single-pointer.js
@@ -25,47 +25,51 @@ class LinkedList {
 
             current.next = newNode;
         }
-
         this.length++;
     }
 
-    isSorted() {
+    conscate(nodes) {
         let current = this.head;
 
         while (current.next != null) {
-            console.log(current.value);
-            if (current.value <= current.next.value) {
-                current = current.next;
-            } else {
-                return false;
-            }
+            current = current.next;
         }
 
-        return true;
+        current.next = nodes.head;
     }
 
     print() {
         let arr = [];
 
-        let current = this.head;
+        let temp = this.head;
 
-        while (current != null) {
-            arr.push(current.value);
-            current = current.next;
+        while (temp != null) {
+            arr.push(temp.value);
+            temp = temp.next;
         }
 
         console.log(arr);
+        return arr;
     }
 }
 
 const linkedList = new LinkedList();
 
-const nums = [1, 2, 3, 4, 5];
+const nums_1 = [1, 2, 3, 4, 5, 6];
 
-for (let n of nums) {
+for (let n of nums_1) {
     linkedList.append(n);
 }
 
+const nodes = new LinkedList();
+
+const nums_2 = [7, 8, 9, 10];
+
+for (let n of nums_2) {
+    nodes.append(n);
+}
+
+console.log(nodes);
+
+linkedList.conscate(nodes);
 linkedList.print();
-const result = linkedList.isSorted();
-console.log(result);
---
+++ js/src/Linked-List/rough.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+    let res = a + b;
+
+    console.log(res);
+    return res;
+}
+
+const a = 12;
+const b = 24;
+
+sum(a, b);
