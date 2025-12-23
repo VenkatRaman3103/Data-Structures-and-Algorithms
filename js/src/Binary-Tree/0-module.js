bbf28373dabc4108422000cace94231c1a50c185
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-12-23 22:38:29 +0530

    update

---
+++ js/src/Binary-Tree/0-module.js
@@ -0,0 +1,59 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    enqueue(val) {
+        const newNode = new ListNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+    }
+
+    dequeue() {
+        if (this.head == null) {
+            return null;
+        }
+
+        let val = this.head.val;
+
+        this.head = this.head.next;
+
+        if (this.head == null) {
+            this.tail = null;
+        }
+
+        this.length -= 1;
+        return val;
+    }
+
+    print() {
+        let res = [];
+
+        let current = this.head;
+
+        while (current != null) {
+            res.push(current.val);
+            current = current.next;
+        }
+
+        console.log(res);
+        return res;
+    }
+}
---
+++ js/src/Binary-Tree/02-preorder-traversal.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    preOrder() {
+        //
+    }
+
+    make(arr) {
+        let root = new Node(arr[0]);
+
+        let q = [];
+        q.push(root);
+
+        let i = 1;
+
+        while (q.length > 0) {
+            let node = q.shift();
+
+            if (arr[i] != null && i < arr.length) {
+                let leftNode = new Node(arr[i]);
+                node.left = leftNode;
+                q.push(node.left);
+            }
+            i++;
+
+            if (arr[i] != null && i < arr.length) {
+                let rightNode = new Node(arr[i]);
+                node.right = rightNode;
+                q.push(node.right);
+            }
+            i++;
+        }
+
+        this.root = root;
+    }
+}
+
+const binaryTree = new BinaryTree();
+const nums = [1, 2, 3, 4, 5, null, 6];
+binaryTree.make(nums);
+
+console.log(binaryTree.root);
