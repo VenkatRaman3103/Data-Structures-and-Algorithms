6dd9652529e8f1f6de2037c37ca8a7efd827f6d7
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-03 17:19:59 +0530

    update

---
+++ js/src/Revision/Linked-list-1/14-middle-node-using-length.js
@@ -0,0 +1,79 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(val) {
+        let newNode = new ListNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let curr = this.head;
+
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+
+            curr.next = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    print() {
+        if (this.head == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.next;
+        }
+
+        console.log(res);
+        return res;
+    }
+
+    middleNode() {
+        if (this.head == null) {
+            return null;
+        }
+
+        let mid = Math.floor(this.length / 2);
+
+        let i = 0;
+
+        let curr = this.head;
+
+        while (curr != null) {
+            curr = curr.next;
+            i += 1;
+
+            if (i == mid) {
+                return curr.val;
+            }
+        }
+    }
+}
+
+const linkedList = new LinkedList();
+
+for (let i = 1; i <= 5; i++) {
+    linkedList.append(i);
+}
+
+linkedList.print();
+console.log(linkedList.middleNode());
---
+++ js/src/Revision/Linked-list-1/16-copy-linked-list.js
@@ -0,0 +1,71 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(val) {
+        let newNode = new ListNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let curr = this.head;
+
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+
+            curr.next = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    print() {
+        if (this.head == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.next;
+        }
+
+        console.log(res);
+        return res;
+    }
+
+    copy() {
+        let dummy = new LinkedList();
+
+        let curr = this.head;
+
+        while (curr != null) {
+            dummy.append(curr.val);
+            curr = curr.next;
+        }
+
+        return dummy;
+    }
+}
+
+const linkedList = new LinkedList();
+
+for (let i = 1; i <= 5; i++) {
+    linkedList.append(i);
+}
+
+linkedList.print();
+console.log(linkedList.copy());
