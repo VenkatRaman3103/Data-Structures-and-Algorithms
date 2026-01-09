50d411e4dde90b92ea7eb9bd021578523cbcaa7a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-09 14:54:22 +0530

    update

---
+++ js/src/Revision/Linked-list-1/26-rotate-linked-list.js
@@ -0,0 +1,84 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(val) {
+        let newNode = new ListNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let curr = this.head;
+
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+
+            curr.next = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    prepend(val) {
+        let newNode = new ListNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    rotate() {
+        let curr = this.head;
+
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        let temp = this.head.next;
+        let last = curr;
+        let first = this.head;
+        this.head.next = null;
+
+        last.next = first;
+        this.head = temp;
+    }
+
+    print() {
+        let res = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.next;
+        }
+
+        console.log(res);
+        return res;
+    }
+}
+
+const linkedList = new LinkedList();
+
+for (let i = 1; i <= 5; i++) {
+    linkedList.append(i);
+}
+
+linkedList.rotate();
+linkedList.rotate();
+linkedList.print();
