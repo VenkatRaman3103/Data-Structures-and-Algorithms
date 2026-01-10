4b2002d6d0f55fbbe18cfbf9ae65aa7294e04fed
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-10 10:13:14 +0530

    Update

---
+++ js/src/Revision/Binary-Tree-1/01-binary-tree.js
@@ -0,0 +1,24 @@
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        //
+    }
+}
