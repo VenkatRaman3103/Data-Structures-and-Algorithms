3ef1795b35fe6a8c695816cf2319120569ad32a4
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-11 23:09:34 +0530

    update

---
+++ js/src/Revision/Binary-Tree-1/05-sum-of-nodes-recursive.js
@@ -0,0 +1,108 @@
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new QueueNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    dequeue() {
+        if (this.head == null) {
+            return 0;
+        }
+
+        let val = this.head.val;
+
+        this.head = this.head.next;
+
+        if (this.head == null) {
+            this.tail = null;
+        }
+
+        this.length -= 1;
+
+        return val;
+    }
+}
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let newNode = new TreeNode(val);
+
+        if (this.root == null) {
+            this.root = newNode;
+            return;
+        }
+
+        let q = new Queue();
+        q.enqueue(this.root);
+
+        while (q.length > 0) {
+            let node = q.dequeue();
+
+            if (node.left != null) {
+                q.enqueue(node.left);
+            } else {
+                node.left = newNode;
+                return;
+            }
+
+            if (node.right != null) {
+                q.enqueue(node.right);
+            } else {
+                node.right = newNode;
+                return;
+            }
+        }
+    }
+
+    sum(root = this.root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return root.val + this.sum(root.left) + this.sum(root.right);
+    }
+}
+
+const b = new BinaryTree();
+
+for (let i = 1; i <= 5; i++) {
+    b.insert(i);
+}
+
+const result = b.sum();
+console.log(result);
