d4a689d91af8362e550f7ecf2335a4dcce098d95
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-11 12:41:43 +0530

    udpate

---
+++ js/src/Revision/Binary-Tree-1/03-print-leaf-node.js
@@ -0,0 +1,53 @@
+import { Queue } from './modules.js';
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let newNode = new TreeNode(val);
+
+        if (this.root == null) {
+            this.root = newNode;
+            return;
+        }
+
+        let q = new Queue();
+        q.enqueue(this.root);
+
+        while (q.length != null) {
+            let node = q.dequeue();
+
+            if (node.left != null) {
+                q.enqueue(node.left);
+            } else {
+                node.left = newNode;
+                return;
+            }
+
+            if (node.right != null) {
+                q.enqueue(node.right);
+            } else {
+                node.right = newNode;
+                return;
+            }
+        }
+    }
+}
+
+const binaryTree = new BinaryTree();
+
+for (let i = 0; i <= 5; i++) {
+    binaryTree.insert(i);
+}
+
+console.log(binaryTree.root);
