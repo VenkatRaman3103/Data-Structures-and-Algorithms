aaef5ef2a70e9058852fc07c8a456f64906ed042
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-11 13:10:54 +0530

    update

---
+++ js/src/Revision/Binary-Tree-1/03-print-left-node-recursive.js
@@ -0,0 +1,53 @@
+import { Queue } from './modules.js';
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let newNode = new TreeNode(val);
+
+        if (this.root == null) {
+            this.root = newNode;
+            return;
+        }
+
+        let q = new Queue();
+        q.enqueue(this.root);
+
+        while (q.length > 0) {
+            let node = q.dequeue();
+
+            if (node.left != null) {
+                q.enqueue(node.left);
+            } else {
+                node.left = newNode;
+                return;
+            }
+
+            if (node.right != null) {
+                node.right = newNode;
+            } else {
+                node.right = newNode;
+                return;
+            }
+        }
+    }
+}
+
+const b = new BinaryTree();
+
+for (let i = 1; i <= 5; i++) {
+    b.insert(i);
+}
+
+console.log(b.root);
