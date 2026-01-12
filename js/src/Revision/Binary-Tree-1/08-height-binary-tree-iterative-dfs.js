5006d7eea2ed7cb6e64bb171567ca4ec5237a944
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-12 12:01:27 +0530

    update

--- js/src/Revision/Binary-Tree-1/08-height-binary-tree-iterative-dfs.js
+++ js/src/Revision/Binary-Tree-1/08-height-binary-tree-iterative-dfs.js
@@ -52,11 +52,71 @@ class Queue {
 
         let res = [];
 
-        let curr = this.head;
+        let curr = this.top;
 
         while (curr != null) {
             res.push(curr.val);
-            curr = curr.next;
+            curr = curr.below;
+        }
+
+        console.log(res);
+        return res;
+    }
+}
+
+class StackNode {
+    constructor(val) {
+        this.val = val;
+        this.below = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new StackNode(val);
+
+        if (this.top == null) {
+            this.top = newNode;
+        } else {
+            newNode.below = this.top;
+            this.top = newNode;
+        }
+
+        this.size += 1;
+    }
+
+    pop() {
+        if (this.top == null) {
+            return null;
+        }
+
+        let val = this.top.val;
+
+        this.top = this.top.below;
+
+        this.size -= 1;
+
+        return val;
+    }
+
+    print() {
+        if (this.top == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.top;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.below;
         }
 
         console.log(res);
@@ -108,10 +168,10 @@ class BinaryTree {
     }
 }
 
-const b = new BinaryTree();
-
-for (let i = 1; i <= 8; i++) {
-    b.insert(i);
-}
-
-console.log(b.root);
+// const b = new BinaryTree();
+//
+// for (let i = 1; i <= 8; i++) {
+//     b.insert(i);
+// }
+//
+// console.log(b.root);
