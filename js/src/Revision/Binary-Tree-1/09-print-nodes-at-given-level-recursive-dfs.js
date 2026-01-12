15cfd365ebaa20b3734ca8bace0a3c6d03270c6f
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-12 21:13:58 +0530

    update

---
+++ js/src/Revision/Binary-Tree-1/09-print-nodes-at-given-level-recursive-dfs.js
@@ -0,0 +1,140 @@
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new QueueNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    dequeue() {
+        if (this.head == null) {
+            return null;
+        }
+
+        let val = this.head.val;
+
+        this.head = this.head.next;
+
+        if (this.head == null) {
+            this.tail = null;
+        }
+
+        this.length -= 1;
+
+        return val;
+    }
+
+    print() {
+        if (this.head == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.next;
+        }
+
+        console.log(res);
+        return res;
+    }
+}
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let newNode = new TreeNode(val);
+
+        if (this.root == null) {
+            this.root = newNode;
+            return;
+        }
+
+        let q = new Queue();
+        q.enqueue(this.root);
+
+        while (q.length > 0) {
+            let node = q.dequeue();
+
+            if (node.left != null) {
+                q.enqueue(node.left);
+            } else {
+                node.left = newNode;
+                return;
+            }
+
+            if (node.right != null) {
+                q.enqueue(node.right);
+            } else {
+                node.right = newNode;
+                return;
+            }
+        }
+    }
+
+    printLevel() {
+        let res = [];
+
+        function fn(root, level) {
+            if (root == null) {
+                return 0;
+            }
+
+            if (res[level] == undefined) {
+                res[level] = [];
+            }
+
+            res[level].push(root.val);
+
+            fn(root.left, level + 1);
+            fn(root.right, level + 1);
+        }
+
+        fn(this.root, 0);
+        return res;
+    }
+}
+
+const b = new BinaryTree();
+
+for (let i = 1; i <= 12; i++) {
+    b.insert(i);
+}
+
+const result = b.printLevel(3);
+console.log(result);
