970722c21817e16676247e75ed67f0220a3810a5
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-12 10:04:10 +0530

    update

---
+++ js/src/Revision/Binary-Tree-1/07-is-exist-iterative.js
@@ -0,0 +1,149 @@
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new QueueNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    dequeue() {
+        if (this.head == null) {
+            return null;
+        }
+
+        let val = this.head.val;
+
+        this.head = this.head.next;
+
+        if (this.head == null) {
+            this.tail = null;
+        }
+
+        this.length -= 1;
+
+        return val;
+    }
+
+    print() {
+        if (this.head == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.next;
+        }
+
+        console.log(res);
+        return res;
+    }
+}
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let newNode = new TreeNode(val);
+
+        if (this.root == null) {
+            this.root = newNode;
+            return;
+        }
+
+        let q = new Queue();
+        q.enqueue(this.root);
+
+        while (q.length > 0) {
+            let node = q.dequeue();
+
+            if (node.left != null) {
+                q.enqueue(node.left);
+            } else {
+                node.left = newNode;
+                return;
+            }
+
+            if (node.right != null) {
+                q.enqueue(node.right);
+            } else {
+                node.right = newNode;
+                return;
+            }
+        }
+    }
+
+    isExist(target) {
+        if (this.root == null && target != null) {
+            return false;
+        }
+
+        if (this.root == null && target == null) {
+            return true;
+        }
+
+        let q = new Queue();
+        q.enqueue(this.root);
+
+        while (q.length > 0) {
+            let node = q.dequeue();
+
+            if (node.val == target) {
+                return true;
+            }
+
+            if (node.left != null) {
+                q.enqueue(node.left);
+            }
+
+            if (node.right != null) {
+                q.enqueue(node.right);
+            }
+        }
+
+        return false;
+    }
+}
+
+const b = new BinaryTree();
+
+for (let i = 1; i <= 5; i++) {
+    b.insert(i);
+}
+
+const result = b.isExist(8);
+console.log(result);
