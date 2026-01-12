4b525cb83a3d0974b261c190c5baaeb353a362ba
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-13 01:00:15 +0530

    update

---
+++ js/src/Revision/Binary-Tree-1/10-postorder-traversal-iterative.js
@@ -0,0 +1,133 @@
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new QueueNode(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length += 1;
+    }
+
+    dequeue() {
+        if (this.head == null) {
+            return null;
+        }
+
+        let val = this.head.val;
+
+        this.head = this.head.next;
+
+        if (this.head == null) {
+            this.tail = null;
+        }
+
+        this.length -= 1;
+
+        return val;
+    }
+
+    print() {
+        if (this.head == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.head;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.next;
+        }
+
+        console.log(res);
+        return res;
+    }
+}
+
+class StackNode {
+    constructor(val) {
+        this.val = val;
+        this.below = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new StackNode(val);
+
+        if (this.top == null) {
+            this.top = newNode;
+        } else {
+            newNode.below = this.top;
+            this.top = newNode;
+        }
+
+        this.size += 1;
+    }
+
+    pop() {
+        if (this.top == null) {
+            return null;
+        }
+
+        let val = this.top.val;
+
+        this.top = this.top.below;
+
+        this.size -= 1;
+
+        return val;
+    }
+
+    print() {
+        if (this.top == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.top;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.below;
+        }
+
+        console.log(res);
+        return res;
+    }
+}
+
+const s = new Stack();
+
+for (let i = 1; i <= 5; i++) {
+    s.push(i);
+}
+
+const removed = s.pop();
+console.log(removed);
+s.print();
