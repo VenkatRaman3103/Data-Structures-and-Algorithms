ce6866298eb8cd0e1d2a28b17f18b5abbf783c8a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-18 02:08:39 +0530

    update

--- js/src/Revision/Binary-Tree-1/rough/binary-tree.js
+++ js/src/Revision/Binary-Tree-1/rough/binary-tree.js
@@ -65,6 +65,67 @@ class Queue {
     }
 }
 
+class StackNode {
+    constructor(val) {
+        this.val = val;
+        this.below = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new StackNode(val);
+
+        if (this.top == null) {
+            this.top = newNode;
+        } else {
+            newNode.below = this.top;
+            this.top = newNode;
+        }
+
+        this.size += 1;
+    }
+
+    pop() {
+        if (this.top == null) {
+            return null;
+        }
+
+        let val = this.top.val;
+
+        this.top = this.top.below;
+
+        this.size -= 1;
+
+        return val;
+    }
+
+    print() {
+        if (this.top == null) {
+            return [];
+        }
+
+        let res = [];
+
+        let curr = this.top;
+
+        while (curr != null) {
+            res.push(curr.val);
+            curr = curr.below;
+        }
+
+        console.log(res);
+
+        return [];
+    }
+}
+
 class TreeNode {
     constructor(val) {
         this.val = val;
@@ -136,21 +197,127 @@ class BinaryTree {
     }
 
     preOrder() {
-        //
+        if (this.root == null) {
+            return null;
+        }
+
+        let res = [];
+
+        let stack = new Stack();
+        stack.push(this.root);
+
+        while (stack.size > 0) {
+            let node = stack.pop();
+
+            res.push(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
+        return res;
     }
 
     inOrder() {
-        //
+        if (this.root == null) {
+            return null;
+        }
+
+        let res = [];
+
+        let stack = new Stack();
+
+        let curr = this.root;
+
+        while (stack.size > 0 || curr != null) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                res.push(curr.val);
+                curr = curr.right;
+            }
+        }
+
+        return res;
     }
 
     postOrder() {
-        //
+        if (this.root == null) {
+            return null;
+        }
+
+        let res = [];
+
+        let stack = new Stack();
+        stack.push(this.root);
+
+        while (stack.size > 0) {
+            let node = stack.pop();
+
+            res.unshift(node.val);
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+
+        return res;
+    }
+
+    levelOrder() {
+        if (this.root == null) {
+            return null;
+        }
+
+        let res = [];
+
+        let q = new Queue();
+        q.enqueue(this.root);
+
+        while (q.length > 0) {
+            let node = q.dequeue();
+
+            res.push(node.val);
+
+            if (node.left != null) {
+                q.enqueue(node.left);
+            }
+
+            if (node.right != null) {
+                q.enqueue(node.right);
+            }
+        }
+
+        return res;
     }
 }
 
 const b = new BinaryTree();
 
-const nodes = [1, 2, 3, null, 5, null, 7];
+const nodes = [1, 2, 3, 4, 5, 6, 7];
 b.make(nodes);
 
 console.log(b.root);
+
+const preOrder = b.preOrder();
+console.log(preOrder);
+
+const inOrder = b.inOrder();
+console.log(inOrder);
+
+const postOrder = b.postOrder();
+console.log(postOrder);
+
+const levelOrder = b.levelOrder();
+console.log(levelOrder);
