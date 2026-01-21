c5b487523ad6a5db3622bc40a896cddc1264e25a
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-01-21 19:37:07 +0530

    update

---
+++ js/src/Graphs/03-dfs/02-adj-matrix/04-recursive-undericted.js
@@ -0,0 +1,39 @@
+class Graph {
+    constructor(n) {
+        this.adjMat = Array.from({ length: n }, () => Array(n).fill(0));
+    }
+
+    addEdge(v1, v2) {
+        this.adjMat[v1][v2] = 1;
+        this.adjMat[v2][v1] = 1;
+    }
+}
+
+const graph = new Graph(5);
+
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 3);
+graph.addEdge(2, 4);
+
+function dfs(graph, start, visited, res) {
+    visited.add(start);
+    res.push(start);
+
+    const children = graph[start];
+
+    for (let i = 0; i < children.length; i++) {
+        let child = children[i];
+
+        if (child == 1 && !visited.has(i)) {
+            dfs(graph, i, visited, res);
+        }
+    }
+
+    return res;
+}
+
+const visited = new Set();
+
+const result = dfs(graph.adjMat, 1, visited, []);
+console.log(result);
