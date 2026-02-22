c23f47c5506c39d6ddfc63b3233ae7c8501f11be
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-02-22 10:18:55 +0530

    solved group anagram (revision, iteration 1) and code refactoring

---
+++ js/src/iterations/iteration_1/04-group-anagram/solution-2-arr.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    let map = {};
+
+    for (let word of strs) {
+        let keyGenArray = new Array(26).fill(0);
+
+        for (let char of word) {
+            let index = char.charCodeAt(0) - 'a'.charCodeAt(0);
+            keyGenArray[index] = '#';
+        }
+
+        let key = keyGenArray.join('');
+
+        if (map[key] == undefined) {
+            map[key] = [word];
+        } else {
+            map[key].push(word);
+        }
+    }
+
+    return Object.values(map);
+};
+
+const strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
+
+const result = groupAnagrams(strs);
+console.log(result);
