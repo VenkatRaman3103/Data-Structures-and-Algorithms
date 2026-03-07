59d1199ea9c02c5c32f5b2ec8fe98394e486545d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-03-07 16:06:27 +0530

    practice hashmap in c

--- c/main.c
+++ c/main.c
@@ -1,26 +1,113 @@
-#include "hashtable/hashtable.h"
+/* #include "hashtable/hashtable.h" */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+#define SIZE 10
 
+typedef struct Node {
+    int val;
+    struct Node *next;
+} Node;
+
+typedef struct HashTable {
+    Node *entries[SIZE];
+} HashTable;
+
+// HASHTABLE
+
+// init
+void initialise(HashTable *hashtable) {
+
+    for (int i = 0; i < SIZE; i++) {
+        hashtable->entries[i] = NULL;
+    }
+}
+
+// hash
+int hash(int num) {
+    int key = num % SIZE;
+    return key;
+}
+
+// insert
+void insert(HashTable *hashtable, int val) {
+    int key = hash(val);
+
+    Node *newNode = malloc(sizeof(Node));
+    newNode->val = val;
+    newNode->next = NULL;
+
+    if (hashtable->entries[key] == NULL) {
+        hashtable->entries[key] = newNode;
+        return;
+    }
+
+    Node *curr = hashtable->entries[key];
+
+    while (curr->next != NULL) {
+        curr = curr->next;
+    }
+
+    curr->next = newNode;
+
+    return;
+}
+
+// delete
+void delete(HashTable *hashtable, int val) {
+    int key = hash(val);
+
+    // deleting index element
+    Node *temp = hashtable->entries[key];
+
+    if (temp == NULL) {
+        return;
+    }
+
+    if (temp->val == val) {
+        hashtable->entries[key] = temp->next;
+        free(temp);
+        return;
+    }
+
+    Node *prev = temp;
+    Node *curr = temp->next;
+
+    while (curr != NULL) {
+
+        if (curr->val == val) {
+            prev->next = curr->next;
+            free(curr);
+            return;
+        }
+
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return;
+}
+
+// search
+// free
+
+int main() {
     HashTable *hashtable = malloc(sizeof(HashTable));
 
-    initialize(hashtable);
-    insert(hashtable, 5);
-    insert(hashtable, 6);
-    insert(hashtable, 7);
-    insert(hashtable, 8);
-    insert(hashtable, 9);
+    initialise(hashtable);
 
-    del(hashtable, 8);
-    print(hashtable);
-
-    printf("%d\n", search(hashtable, 12));
     insert(hashtable, 12);
-    printf("%d\n", search(hashtable, 12));
+    insert(hashtable, 3);
 
-    freeTable(hashtable);
+    printf("%d\n", hashtable->entries[2]->val);
+
+    delete(hashtable, 12);
+
+    if (hashtable->entries[2]) {
+        printf("%d\n", hashtable->entries[2]->val);
+    }
+
+    free(hashtable);
 
     return 0;
 }
