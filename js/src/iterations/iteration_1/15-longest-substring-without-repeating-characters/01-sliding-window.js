7d9e0c9c98491129a4ac83054ae8007484ed6f25
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-03-12 19:23:37 +0530

    revision

--- js/src/iterations/iteration_1/15-longest-substring-without-repeating-characters/01-sliding-window.js
+++ js/src/iterations/iteration_1/15-longest-substring-without-repeating-characters/01-sliding-window.js
@@ -3,7 +3,31 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    //
+    let chars = s.split('');
+
+    let longest = 0;
+
+    for (let i = 0; i < chars.length; i++) {
+        let hashTable = {};
+
+        for (let j = i; j < chars.length; j++) {
+            let key = chars[j];
+
+            if (hashTable[key] == undefined) {
+                hashTable[key] = 1;
+            } else {
+                break;
+            }
+        }
+
+        if (longest < Object.keys(hashTable).length) {
+            longest = Object.keys(hashTable).length;
+        }
+
+        hashTable = {};
+    }
+
+    return longest;
 };
 
 const s = 'abcabcbb';
