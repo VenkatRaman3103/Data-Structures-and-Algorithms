cc7c90339274747355647d74bc249a250a6ed5be
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-03-19 23:05:56 +0530

    revision productExceptSelf using prefix and postfix (iteration 2)

---
+++ js/src/iterations/iteration_2/06-product-of-array-except-self/01-prefix-postfix.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    let result = Array.from({ length: nums.length }).fill(1);
+
+    let prefix = 1;
+    let prefixArr = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let n = nums[i];
+
+        prefix *= n;
+        prefixArr.push(prefix);
+    }
+
+    let postfix = 1;
+    let postfixArr = [];
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        let n = nums[i];
+
+        postfix *= n;
+        postfixArr.unshift(postfix);
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        let a = 1;
+        let b = 1;
+
+        if (prefixArr[i - 1] == undefined) {
+            a = 1;
+        } else {
+            a = prefixArr[i - 1];
+        }
+
+        if (postfixArr[i + 1] == undefined) {
+            b = 1;
+        } else {
+            b = postfixArr[i + 1];
+        }
+
+        let key = a * b;
+
+        if (key == '-0') {
+            key = 0;
+        }
+
+        result[i] = key;
+    }
+
+    return result;
+};
+
+const nums = [1, 2, 3, 4];
+
+const result = productExceptSelf(nums);
+console.log(result);
