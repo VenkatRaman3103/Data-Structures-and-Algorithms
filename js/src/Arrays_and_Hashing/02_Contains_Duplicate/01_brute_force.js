34eadc58c0d574d985097420619551dac316c5b6
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-05-07 19:57:48 +0530

    completed: Contains Duplicate using brute force

---
+++ js/src/Arrays_and_Hashing/02_Contains_Duplicate/01_brute_force.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i != j) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+};
+
+const nums = [0, 2, 3, 1];
+
+const result = containsDuplicate(nums);
+console.log(result);
