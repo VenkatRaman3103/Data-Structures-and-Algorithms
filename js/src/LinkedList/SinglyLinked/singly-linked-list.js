5fb1630b312849985860e5c31350b3b7b627739c
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-09-07 23:47:20 +0530

    Linked List revision

--- js/src/LinkedList/SinglyLinked/singly-linked-list.js
+++ js/src/LinkedList/SinglyLinked/singly-linked-list.js
@@ -1,3 +1,5 @@
+import { loadESLint } from 'eslint';
+
 class Node {
     constructor(val) {
         this.val = val;
@@ -8,15 +10,57 @@ class Node {
 class LinkedList {
     constructor() {
         this.head = null;
-
-        this.size = 0;
+        this.length = 0;
     }
 
     append(val) {
-        //
+        let newNode = new Node(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            let curr = this.head;
+
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+
+            curr.next = newNode;
+        }
+
+        this.length += 1;
     }
 
     prepend(val) {
         //
     }
+
+    removeFirst() {
+        //
+    }
+
+    removeLast() {
+        //
+    }
+
+    print() {
+        if (this.head == null) {
+            return null;
+        }
+
+        let curr = this.head;
+
+        while (curr != null) {
+            console.log(curr.val);
+            curr = curr.next;
+        }
+    }
 }
+
+const linkedlist = new LinkedList();
+
+for (let i = 1; i <= 10; i++) {
+    linkedlist.append(i);
+}
+
+linkedlist.print();
