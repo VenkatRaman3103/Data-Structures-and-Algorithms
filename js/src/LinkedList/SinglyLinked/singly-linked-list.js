85af6803584e45e5df17b521d83d4be143b01e1c
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-09-07 23:15:03 +0530

    update

--- js/src/LinkedList/SinglyLinked/singly-linked-list.js
+++ js/src/LinkedList/SinglyLinked/singly-linked-list.js
@@ -1,5 +1,22 @@
 class Node {
     constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+
+        this.size = 0;
+    }
+
+    append(val) {
+        //
+    }
+
+    prepend(val) {
         //
     }
 }
