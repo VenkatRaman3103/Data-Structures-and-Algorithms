8cb29cd817e957ee13c2190275e50c65ce8111fe
Venkat Raman <venkat.raman.3103@gmail.com>
2026-09-16 23:38:22 +0530

    Implement array-based circular queue

---
+++ js/src/Queue/02-array-based-fixed-capacity-circular-queue.js
@@ -0,0 +1,36 @@
+class ArrayQueue {
+    constructor(capacity) {
+        this.queue = new Array(capacity)
+        this.capacity = capacity
+        this.front = 0
+        this.rear = 0
+        this.size = 0
+    }
+
+    enqueue(val) {
+        if (this.isFull()) {
+            return null
+        }
+        this.queue[this.rear] = val
+        this.rear = (this.rear + 1) % this.capacity
+        this.size += 1
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return null
+        }
+        let val = this.queue[this.front]
+        this.front = (this.front + 1) % this.capacity
+        this.size -= 1
+        return val
+    }
+
+    isEmpty() {
+        return this.size == 0
+    }
+
+    isFull() {
+        return this.size == this.capacity
+    }
+}
