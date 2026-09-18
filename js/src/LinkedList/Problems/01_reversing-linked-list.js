371369cca1cce176427072d363bcd778717398d2
Venkat Raman <venkat.raman.3103@gmail.com>
2026-09-18 22:51:36 +0530

    Implement function to reverse a linked list

---
+++ js/src/LinkedList/Problems/01_reversing-linked-list.js
@@ -0,0 +1,13 @@
+function reverseList(head) {
+    let prev = null
+    let curr = head
+
+    while (curr != null) {
+        let temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    }
+
+    return prev
+}
